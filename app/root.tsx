c7e9e77db96ddc757b1140b28534532d7bf218d9
betothewizard <betothewizard@gmail.com>
2025-06-25 23:01:01 +0700

    feat: matsu theme

--- app/root.tsx
+++ app/root.tsx
@@ -86,6 +86,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <div className="texture" />
         {children}
         <ScrollRestoration />
         <Scripts />
